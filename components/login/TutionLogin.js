eca929e61cca7facb050e6ac99ca15bac6e0b529
n48331 <unfolks@gmail.com>
2022-10-30 23:11:44 +0530

    login

---
+++ components/login/TutionLogin.js
@@ -0,0 +1,43 @@
+import styles from "./ilogin.module.css";
+
+export default function Ilogin() {
+  return (
+    <>
+      <section className={styles.login}>
+        <div></div>
+        <div className={styles.fields}>
+          <form action="">
+            <div className={styles.heading}>
+              <div className={styles.image}></div>
+              <div>
+                <h1>ELITE TUTION</h1>
+                <p>District, Kerala</p>
+              </div>
+            </div>
+            <div className={styles.subhead}>
+              <h3>Welcome Elite Student!</h3>
+              <p>Enter your username and password</p>
+            </div>
+            <div className={styles.inputs}>
+              <div>
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" />
+              </div>
+              <div>
+                <label htmlFor="pwd">Password</label>
+                <input type="password" name="pwd" />
+                <p>Forgot Password?</p>
+              </div>
+              <div>
+                <button>Login</button>
+                <p>
+                  Don’t have an account? <span>Sign Up</span>
+                </p>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ pages/institute-login.js
@@ -0,0 +1,8 @@
+import Ilogin from "../components/login/TutionLogin";
+export default function Home() {
+  return (
+    <>
+      <Ilogin />
+    </>
+  );
+}
