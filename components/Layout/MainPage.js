444b5eda24d2daf2ca9434841aff3e946e6316e7
n48331 <unfolks@gmail.com>
2023-01-10 01:55:19 +0530

    profile

--- components/Layout/MainPage.js
+++ components/Layout/MainPage.js
@@ -1,11 +1,24 @@
-import TimeTable from "../dashboard/TimeTable";
 import SideBar from "../common/SideBar";
+import TimeTable from "../dashboard/TimeTable/TimeTable";
+import styles from "./mainpage.module.css"
+import Profile from "../dashboard/Profile/Profile"
 
 const MainPage = () => {
   return (
     <>
+    <div className={styles.dashboard}>
+      <div>
       <SideBar />
+      </div>
+      <div className={styles.container}>
+        <div>
       <TimeTable />
+        </div>
+        <div>
+          <Profile/>
+        </div>
+      </div>
+    </div>
     </>
   );
 };
--- components/dashboard/TimeTable.js
+++
@@ -1,9 +0,0 @@
-import styles from "./dashboard.module.css";
-
-export default function TimeTable() {
-  return (
-    <>
-      <section className={styles.dashboard}>Dashboard</section>
-    </>
-  );
-}
